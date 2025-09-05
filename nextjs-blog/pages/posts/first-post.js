b37ddbbc4663379443bd3f482f34cda19754f374
Andrew Mayfield <batgnome@gmail.com>
2025-09-05 16:18:58 -0700

    made a custom css for post component

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../../components/layout';
+import styles from '/styles/post.module.css';
 
  
 export default function FirstPost() {
@@ -10,10 +11,11 @@ export default function FirstPost() {
         {/* Page title for the first post */}
         <title>First Post</title>
       </Head>
-
+      <section className={styles.post}>
       {/* Main heading for the first post */}
       <h1>First Post</h1>
-
+      <p>This is a post, very cool. Then we talk some more about something, very intriguing.</p>
+      </section>
     </Layout>
   );
 }
